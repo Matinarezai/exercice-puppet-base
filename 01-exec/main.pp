363d5e73a103415077976bfbcae478d51a132b35
matinarezai <rezaimatin72@gmail.com>
2023-01-30 15:57:36 +0100

    un petit jeux de chiffre pour tester !

--- 01-exec/main.pp
+++ 01-exec/main.pp
@@ -1,16 +1,32 @@
 #!/bin/bash
-nombre_secret=$(($RANDOM % 100 + 1))
-tentatives=0
 
-echo "Devinez le nombre entre 1 et 100:"
+reponses_justes=0
 
-while read -p "Votre devinette: " devinette && [ "$devinette" != "$nombre_secret" ]; do
-  if [ "$devinette" -lt "$nombre_secret" ]; then
-    echo "C'est plus!"
+for i in {1..5}; do
+  nombre1=$(($RANDOM % 10 + 1))
+  nombre2=$(($RANDOM % 10 + 1))
+  resultat=$((nombre1 + nombre2))
+
+  echo "Quel est le résultat de $nombre1 + $nombre2 ?"
+  read reponse
+
+  if [ "$reponse" == "$resultat" ]; then
+    echo "Correct!"
+    reponses_justes=$((reponses_justes + 1))
   else
-    echo "C'est moins!"
+    echo "Incorrect. La réponse correcte est $resultat."
   fi
-  tentatives=$((tentatives + 1))
 done
 
-echo "Bravo! Vous avez trouvé le nombre en $tentatives tentatives."
+if [ "$reponses_justes" -lt "5" ]; then
+  echo "Vous avez perdu! Étendant l'écran pendant 30 secondes."
+  xrandr --output eDP1 --auto --right-of HDMI1
+  sleep 30
+  xrandr --output eDP1 --auto --primary
+else
+  echo "Félicitations! Vous avez gagné! Étendant l'écran pendant 10 secondes."
+  xrandr --output eDP1 --auto --right-of HDMI1
+  sleep 10
+  xrandr --output eDP1 --auto --primary
+fi
+
