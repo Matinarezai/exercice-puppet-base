b50522e72ed0c5cdef37767b7cb0956d4a98e790
test <rezaimatin72@gmail.com>
2024-02-05 17:18:51 +0100

    utilisateur avec uid

--- 05-user-group/main.pp
+++ 05-user-group/main.pp
@@ -2,8 +2,8 @@
 user { 'c2c':
   ensure     => 'present',     # Assure que l'utilisateur existe
   managehome => true,          # Gère également le répertoire personnel de l'utilisateur
-  uid        => '1001',        # UID de l'utilisateur
-  gid        => 'users',       # GID de l'utilisateur (groupe)
+  uid        => '1002',        # UID de l'utilisateur
+  gid        => '1002',       # GID de l'utilisateur (groupe)
   home       => '/home/c2c',   # Chemin du répertoire personnel
   shell      => '/bin/bash',    # Shell de l'utilisateur
   password   => '$1$abcd1234$56789',  # Mot de passe chiffré (utilisez 'openssl passwd -1' pour générer)
