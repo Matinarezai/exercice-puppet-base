3449c1e6cf7181596fbab8f4913df838985e482c
test <rezaimatin72@gmail.com>
2024-02-05 16:23:03 +0100

    utilisateur avec uid

--- 05-user-group/main.pp
+++ 05-user-group/main.pp
@@ -1,15 +1,19 @@
-group { 'c2c':
-  ensure => 'present',
-  gid    => 1001,
-}
+class user_group {
+  $password = file('/path/to/password.txt')
 
-user { 'c2c':
-  ensure     => 'present',
-  password   => sensitive('ztrewq'),   
-  uid        => 1001,     
-  gid        => 'c2c',  # Use the group name, not the GID
-  home       => '/home/c2c',  
-  shell      => '/bin/bash',
-  managehome => true,
+  group { 'c2c':
+    ensure => 'present',
+    gid    => 1001,
+  }
+
+  user { 'c2c':
+    ensure     => 'present',
+    password   => $password,
+    uid        => 1001,
+    gid        => 'c2c',
+    home       => '/home/c2c',
+    shell      => '/bin/bash',
+    managehome => true,
+  }
 }
 
