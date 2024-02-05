cfc040b84519edf3c966f07e34d3b8c710757305
test <rezaimatin72@gmail.com>
2024-02-05 15:20:17 +0100

    utilisateur avec uid

--- 05-user-group/main.pp
+++ 05-user-group/main.pp
@@ -1,9 +1,15 @@
+group { 'c2c':
+  ensure => 'present',
+  gid    => 1001,
+}
+
 user { 'c2c':
-  ensure     => 'present', 
+  ensure     => 'present',
+  password   => sensitive('ztrewq'),   
   uid        => 1001,     
-  gid        => '1001', 
+  gid        => 'c2c',  # Use the group name, not the GID
   home       => '/home/c2c',  
   shell      => '/bin/bash',
-  managehome => true
+  managehome => true,
 }
 
