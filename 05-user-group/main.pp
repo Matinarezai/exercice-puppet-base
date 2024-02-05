40c4ec623ea25a6bd51dedbd820d83a4d4bb179d
Matin Rezai <mrezai@wrk69.wrk.lsn.camptocamp.com>
2024-02-05 15:08:46 +0100

    correct the username of blegoff in puppet-c2c

--- 05-user-group/main.pp
+++ 05-user-group/main.pp
@@ -1,9 +1,10 @@
 user { 'c2c':
   ensure     => 'present',
-  password   => sensitive ("ztrewq")   
+  password   => sensitive('ztrewq'),   
   uid        => 1001,     
   gid        => '1001', 
   home       => '/home/c2c',  
   shell      => '/bin/bash',
   managehome => true,
 }
+
