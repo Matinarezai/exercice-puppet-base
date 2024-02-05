ffadd6e1d1bf1fc3a27f50e14ad8cfde485d041e
test <rezaimatin72@gmail.com>
2024-02-05 17:17:00 +0100

    utilisateur avec uid

--- 05-user-group/main.pp
+++ 05-user-group/main.pp
@@ -4,9 +4,9 @@ user { 'c2c':
   managehome => true,          # Gère également le répertoire personnel de l'utilisateur
   uid        => '1001',        # UID de l'utilisateur
   gid        => 'users',       # GID de l'utilisateur (groupe)
-  home       => '/home/c2c',  # Chemin du répertoire personnel
+  home       => '/home/c2c',   # Chemin du répertoire personnel
   shell      => '/bin/bash',    # Shell de l'utilisateur
-  password   => 'qwertz',  # Mot de passe chiffré (utilisez 'openssl passwd -1' pour générer)
+  password   => '$1$abcd1234$56789',  # Mot de passe chiffré (utilisez 'openssl passwd -1' pour générer)
 }
 
 # Vous pouvez également ajouter d'autres configurations ou exécutions de commandes en fonction de vos besoins.
