c3e8e1046782d77926bb9bb4e9a8dd1ee17f9b4a
test <rezaimatin72@gmail.com>
2024-02-05 14:31:54 +0100

    utilisateur avec uid

--- 01-exec/main.pp
+++ 01-exec/main.pp
@@ -2,3 +2,5 @@ exec { 'install_fish':
   command => 'apt update && apt install -y fish',
   unless  => 'dpkg -l | grep fish',
 }
+
+test
--- 05-user-group/main.pp
+++ 05-user-group/main.pp
@@ -1,8 +1,9 @@
-user { 'example_user':
-  ensure     => 'present',   # You can also use 'absent' to ensure the user is removed
-  uid        => 1001,        # Specify the desired UID
-  gid        => 'example_group',  # Specify the primary group
-  home       => '/home/example_user',  # Specify the home directory
-  shell      => '/bin/bash',  # Specify the login shell
-  managehome => true,         # Manage home directory (create or remove)
+user { 'c2c':
+  ensure     => 'present',
+  password   => sensitive ("ztrewq")   
+  uid        => 1001,     
+  gid        => '1001', 
+  home       => '/home/c2c',  
+  shell      => '/bin/bash',
+  managehome => true,
 }
