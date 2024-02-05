9fbce48d19e723d1fa9c4726238717bf8b6a5ead
test <rezaimatin72@gmail.com>
2024-02-05 17:12:04 +0100

    utilisateur avec uid

--- 05-user-group/main.pp
+++ 05-user-group/main.pp
@@ -1,12 +1,12 @@
 # Définition de l'utilisateur
-user { 'nom_utilisateur':
+user { 'c2c':
   ensure     => 'present',     # Assure que l'utilisateur existe
   managehome => true,          # Gère également le répertoire personnel de l'utilisateur
   uid        => '1001',        # UID de l'utilisateur
   gid        => 'users',       # GID de l'utilisateur (groupe)
-  home       => '/home/nom_utilisateur',  # Chemin du répertoire personnel
+  home       => '/home/c2c',  # Chemin du répertoire personnel
   shell      => '/bin/bash',    # Shell de l'utilisateur
-  password   => '$1$abcd1234$56789',  # Mot de passe chiffré (utilisez 'openssl passwd -1' pour générer)
+  password   => 'qwertz',  # Mot de passe chiffré (utilisez 'openssl passwd -1' pour générer)
 }
 
 # Vous pouvez également ajouter d'autres configurations ou exécutions de commandes en fonction de vos besoins.
